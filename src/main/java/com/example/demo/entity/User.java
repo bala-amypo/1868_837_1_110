64484a80aa1773074d788e8b4c2cfcdc28472e20
bala-amypo <bala@amypo.in>
2025-12-24 13:50:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -33,9 +33,6 @@ public class User {
 
     // getters & setters
     public Long getId() { return id; }
-    public void setId(Long id) {
-    this.id = id;
-}
     public void setId(Long id) { this.id = id; }
     public String getFullName() { return fullName; }
     public void setFullName(String fullName) { this.fullName = fullName; }
