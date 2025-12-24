468f0fb4d380dc4fad7b780814d74f74f3118c0a
bala-amypo <bala@amypo.in>
2025-12-24 04:02:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/Resource.java
+++ src/main/java/com/example/demo/entity/Resource.java
@@ -4,24 +4,9 @@ import jakarta.persistence.*;
 
 @Entity
 public class Resource {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
-
     private String resourceName;
     private String resourceType;
-    private int quantity;
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getResourceName() { return resourceName; }
-    public void setResourceName(String resourceName) { this.resourceName = resourceName; }
-
-    public String getResourceType() { return resourceType; }
-    public void setResourceType(String resourceType) { this.resourceType = resourceType; }
-
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
 }
